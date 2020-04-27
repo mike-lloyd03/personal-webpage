960996b9bcb58a8cdf31c94c40e665b95c8313da
mike <michael.lloyd03@gmail.com>
2020-04-26 23:06:05 -0700

    basic stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import {Link, Switch, Route} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+
 import './App.css';
 
 function App() {
   return (
-    <nav>
-      
-    </nav>
+    <div>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+      </nav>
+
+      <main>
+        <Switch>
+          <Route exact path='/'><Home /></Route>
+          <Route path='/about'><About /></Route>
+          <Route path='/contact'><Contact /></Route>
+        </Switch>
+      </main>
+    </div>
   );
 }
 
