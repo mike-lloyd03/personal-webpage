0c53e25597c4a8872a213f2f0dae7b3aebf4d712
mike <michael.lloyd03@gmail.com>
2020-05-07 22:10:26 -0700

    added project grid cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom'
+import {Paper} from '@material-ui/core'
 import 'remixicon/fonts/remixicon.css'
 
 import Nav from './Nav/Nav'
 import About from './pages/About/About'
 import Projects from './pages/Projects/Projects'
 import ProjectDetail from './pages/Projects/ProjectDetail'
-import projectsData from './pages/Projects/projectsData'
 import Calculator from './pages/Projects/Calculator/Calculator'
 
 import './App.css';
@@ -19,12 +19,14 @@ function App() {
       <Nav />
 
       <main>
-        <Switch>
-          <Route exact path='/'><About /></Route>
-          <Route path='/projects/calculator'><Calculator /></Route>
-          <Route path='/projects/:projectID'><ProjectDetail /></Route>
-          <Route path='/projects'><Projects /></Route>
-        </Switch>
+        <Paper className='mainContent'>
+          <Switch>
+            <Route exact path='/'><About /></Route>
+            <Route path='/projects/calculator'><Calculator /></Route>
+            <Route path='/projects/:projectID'><ProjectDetail /></Route>
+            <Route path='/projects'><Projects /></Route>
+          </Switch>
+        </Paper>
       </main>
     </div>
   );
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,40 +1,36 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import {Card, CardContent, CardMedia, CardActionArea, Breadcrumbs} from '@material-ui/core'
 
 import projectsData from './projectsData'
+import './Projects.css'
 
 export default function Projects() {
-  const projectTitles = projectsData.map(p => 
-    <li key={p.id}>
-      <Link
-        to={'/projects/' + p.id}
-      >
-        {p.title}
-      </Link>
-    </li>
+
+  const projectCards = projectsData.map(p => 
+    <Card key={p.id}>
+      <CardActionArea>
+        <Link to={`/projects/${p.id}`}>
+          <CardMedia
+            component='img'
+            image={require(`./images/${p.id}/main-img.jpg`)}
+            height='200'
+          />
+        </Link>
+      </CardActionArea>
+        
+        <CardContent>
+          {p.description}
+        </CardContent>
+    </Card>
     )
   
   return (
     <div>
       <h1>Projects</h1>
-      <ul>
-        {projectTitles}
-        <li><Link to='/projects/calculator'>Calculator</Link></li>
-      </ul>
-      <Card>
-        <CardMedia
-          image='src/pages/Projects/images/bass-body-main.jpg'
-        />
-        
-        <CardContent>
-          I built a guitar
-        </CardContent>
-
-      </Card>
-      
+      <div className='projectsContainer'>
+        {projectCards}
+      </div>
     </div>
   )
 }
